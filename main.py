7a01c41fe80b952105c305b67a09dec97cc4bd6f
Patryk Surmacz <patryksurmacz@gmail.com>
2023-05-25 22:08:39 +0200

    interface update

--- main.py
+++ main.py
@@ -9,37 +9,45 @@ import gradio as gr
 # Set the value of the OPENAI_API_KEY variable to the value of the environment variable "OPENAI_API_KEY"  # noqa: E501
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
 
-# Supplying a persist_directory will store the embeddings on disk
+# Supplying a persist_directory will store the embeddings on disk. This will create a new folder in the direcotry where this script is located  # noqa: E501
 persist_directory = 'db'
 
 # Initialize the OpenAIEmbeddings module
 embeddings = OpenAIEmbeddings()
 
-# Load the persisted vector store from disk
+# Load the vector store from disk
 vectordb = Chroma(persist_directory=persist_directory, embedding_function=embeddings)
 
 # Use the ChatVectorDBChain module to generate an answer to the user's input based on the loaded vector store and the OpenAI language model  # noqa: E501
 MyOpenAI = OpenAI()
 qa = ChatVectorDBChain.from_llm(MyOpenAI, vectordb)
 
-def chatbot(input_text):
+def chatbot(your_input):
     # Add user input to message history
-    chat_history.append("User: " + input_text)
+    chat_history.append("User: " + your_input)
     # Generate an answer to the user's input using the ChatVectorDBChain module
-    response = qa({"question": input_text, "chat_history": message_history})
+    response = qa({"question": your_input, "chat_history": message_history})
     print(response["answer"])
     # Add response to message history
     chat_history.append("Chatbot: " + response["answer"])
     # Return response
     return response["answer"] + "\n\n" + "\n".join(chat_history)
 
-# Initialize message history
+"""
+Initialize message history and chat history. These must be separate, 
+Message history is used by the ChatVectorDBChain module to generate an answer to the user's input
+Chat history is used to display the full chat history to the user
+"""  # noqa: E501
+
 message_history = []
 chat_history = []
 
 if __name__ == "__main__":
-    # Define Gradio interface
-    iface = gr.Interface(fn=chatbot, inputs="text", outputs="text", title="Gradio Chat App", 
+    # Define Gradio interface and its boxes
+    text = gr.inputs.Textbox(lines=1, label="Message")
+    textbox = gr.inputs.Textbox(lines=10, label="Message History")
+
+    iface = gr.Interface(fn=chatbot, inputs=text, outputs=textbox, title="Document Chat App", 
                         description="Enter a message to chat with the chatbot. Full message history will be displayed below.")
 
     # Run the interface
